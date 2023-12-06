d531286773aa842bc7750297366294798037167c
kodxxl <xsrc@mail.ru>
2023-12-06 08:39:20 +0400

    modified:   configure.go

--- configure.go
+++ configure.go
@@ -1,5 +1,15 @@
 package microservice
 
-func configure() {
+const envPrefix string = "MYSERVICE"
+
+type config struct {
+	LISTEN_ADDERSS string
+	LISTEN_PORT string
+
+	STORAGE_ADDRESS string
+	STORAGE_PORT string
+}
+
+func Configure() {
 
 }
