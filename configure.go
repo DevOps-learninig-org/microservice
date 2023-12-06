1ebced86d712c38f195709b22ab9d567183624cc
kodxxl <111854579+kodxxl@users.noreply.github.com>
2023-12-06 10:16:01 +0400

    Update configure.go

--- configure.go
+++ configure.go
@@ -4,8 +4,6 @@ import (
 
 )
 
-const envPrefix string = "MYSERVICE"
-
 type Config struct {
 	LISTEN_ADDERSS string
 	LISTEN_PORT string
@@ -18,16 +16,16 @@ type configFieldsTypes interface {
 	int | string
 }
 
-func setConfigField[ T configFieldsTypes ]( field *T, defaultVal T ) {
+func setConfigField[ T configFieldsTypes ]( prefix string, field *T, defaultVal T ) {
 	
 }
 
-func Configure() (Config, error) {
+func Configure(envPrefix string) (Config, error) {
         result := Config{}
-	setConfigField[string]( &result.LISTEN_ADDERSS, "127.0.0.1" )
-	setConfigField[string]( &result.LISTEN_PORT, "3000" )
-	setConfigField[string]( &result.STORAGE_ADDRESS, "127.0.0.1" )
-	setConfigField[string]( &result.STORAGE_PORT, "3306" )
+	setConfigField[string]( envPrefix, &result.LISTEN_ADDERSS, "127.0.0.1" )
+	setConfigField[string]( envPrefix, &result.LISTEN_PORT, "3000" )
+	setConfigField[string]( envPrefix, &result.STORAGE_ADDRESS, "127.0.0.1" )
+	setConfigField[string]( envPrefix, &result.STORAGE_PORT, "3306" )
 	
         return result, nil
 }
