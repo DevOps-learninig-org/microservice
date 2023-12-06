35df5407fd35bad6dcd28a513f319f6779459e97
kodxxl <xsrc@mail.ru>
2023-12-06 09:40:04 +0400

    Some funcionaliti in configure.go

--- configure.go
+++ configure.go
@@ -1,8 +1,12 @@
 package microservice
 
+import (
+  "reflect"
+)
+
 const envPrefix string = "MYSERVICE"
 
-type config struct {
+type Config struct {
 	LISTEN_ADDERSS string
 	LISTEN_PORT string
 
@@ -10,6 +14,25 @@ type config struct {
 	STORAGE_PORT string
 }
 
-func Configure() {
+func getVar() {
 
+}
+
+func Configure() (Config, error) {
+        result := Config{
+		LISTEN_ADDERSS:		"127.0.0.1",
+		LISTEN_PORT:		"3000",
+
+		STORAGE_ADDRESS:	"127.0.0.1",
+		STORAGE_PORT:		"3306",
+	}
+
+	v := reflect.ValueOf(result)
+	f := v.Type()
+
+        for i := 0; i < v.NumField(); i++ {
+		f.Field(i).Name
+	}
+
+        return result, nil
 }
